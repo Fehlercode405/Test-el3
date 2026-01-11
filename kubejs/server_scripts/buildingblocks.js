2afef2406d3a16b096d26a3d504055ebf6910ccc
ERROR <jan+github@schulte-krude.de>
2026-01-11 15:32:54 +0100

    1.2.0 (relase) -smallfixes -recpies

--- kubejs/server_scripts/buildingblocks.js
+++ kubejs/server_scripts/buildingblocks.js
@@ -1,7 +1,22 @@
-/*ServerEvents.recipes(event => {
+ServerEvents.recipes(event => {
     event.recipes.mekanism.combining(
-        "create:asurine",
-        add
+        "2x create:asurine",
+        "moremekanismprocessing:dust_zinc",
+        "2x minecraft:cobblestone",
+    )//nugets von einem Block: 3  Verhältnis:0,66
+        event.recipes.mekanism.combining(
+        "3x create:ochrum",
+        "mekanism:dust_gold",
+        "3x minecraft:cobblestone",
+    )//nugets von einem Block: 2  Verhältnis:0,66
+    event.recipes.mekanism.combining(
+        "create:crimsite",
+        "mekanism:dust_iron",
         "minecraft:cobblestone",
-    )
-})unfished*/
+    )//nugets von einem Block: 4  Verhältnis:0,44
+    event.recipes.mekanism.combining(
+        "2x create:veridium",
+        "3x mekanism:dust_copper",
+        "3x minecraft:cobblestone"
+    ) //nugets von einem Block: 8  Verhältnis:0,59
+})
