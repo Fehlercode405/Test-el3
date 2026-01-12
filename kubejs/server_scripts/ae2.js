e08325588e767f2c2b0720f7b2e941b455e11597
ERROR <jan+github@schulte-krude.de>
2026-01-12 17:51:49 +0100

    Fixed recpies

--- kubejs/server_scripts/ae2.js
+++ kubejs/server_scripts/ae2.js
@@ -5,28 +5,28 @@ ServerEvents.recipes(event => {
         "ae2:printed_engineering_processor",
         "mekanism:alloy_infused",
         "ae2:printed_silicon",
-    )
+    ).mode("press")
     event.remove({ id: 'ae2:inscriber/calculation_processor' })
     event.recipes.ae2.inscriber(
         "ae2:calculation_processor",
         "ae2:printed_calculation_processor",
         "mekanism:alloy_infused",
         "ae2:printed_silicon",
-    )
+    ).mode("press")
     event.remove({ id: 'advanced_ae:quantum_processor' })
     event.recipes.ae2.inscriber(
         "advanced_ae:quantum_processor",
         "advanced_ae:printed_quantum_processor",
         "mekanism:alloy_infused",
         "ae2:printed_silicon",
-    )
+    ).mode("press")
     event.remove({ id: 'ae2:inscriber/logic_processor' })
     event.recipes.ae2.inscriber(
         "ae2:logic_processor",
         "ae2:printed_logic_processor",
         "mekanism:alloy_infused",
         "ae2:printed_silicon",
-    )
+    ).mode("press")
     event.remove({id: "advanced_ae:regeneration_card"})
     event.shaped(
         Item.of('advanced_ae:regeneration_card'),
